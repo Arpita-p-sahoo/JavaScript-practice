d0de3f52326edf42eb1ed6765678a2a45d90734e
Arpita Sahoo <142164739+Arpita-p-sahoo@users.noreply.github.com>
2026-08-10 12:11:39 +0000

    datatypes discussed

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12324343434;
+let accountEmail = "arpita@gmail.com";
+var accountPassword = "123456";
+accountCity = "bhubaneswar"
+let accountState;
+
+// prefer not to use var for its block scoped and function scoped issues
+
+accountEmail = "ap@ap.com"
+accountPassword = "32121"
+accountCity = "ctc"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ .devcontainer/01_basics/02_datatyps.js
@@ -0,0 +1,29 @@
+ "use stricts"; //treats all the javascript code as newer version
+
+//  alert("hello") //we are using nodejs
+
+ console.log(3-2);
+
+ console.log("code readability is super important" );
+ 
+ let name = "Arpita" //string data type
+ let age = 27 // number data type
+ let isLoggedin = false //boolean
+
+ /**
+  * string - "Arpita"
+  * number - 12312
+  * boolean - true/false
+  * null - this is a standalone value 
+  * undefined - value has not assigned yet
+  * symbol - unique
+  * 
+  * object
+  */
+
+//  console.log(typeof null); - object
+// console.log(typeof undefined); - undefined
+
+
+
+ 
