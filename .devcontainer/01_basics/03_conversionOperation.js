dade74e86dcc3b174f5aef7f94699c11c6058d62
Arpita Sahoo <142164739+Arpita-p-sahoo@users.noreply.github.com>
2026-08-10 12:28:25 +0000

    conversion operation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+/**let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log( valueInNumber); // NaN(Not a number) - type of NaN is number
+
+
+ * true - Number - 1
+ * false - Number - 0
+ * null - Number - 0
+ * undefined - Number - NaN
+ * 
+ */
+
+// let isLoggedIn = "";
+// console.log(Boolean(isLoggedIn));
+
+/**
+ * 1 - true
+ * "" = false
+ * 0 - false
+ * "Arpita" - true
+ */
+
+// let someNumber = 77
+// console.log(typeof String(someNumber));
