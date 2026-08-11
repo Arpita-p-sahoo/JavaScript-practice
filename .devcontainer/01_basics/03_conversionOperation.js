3a8c92cfc30a6e0d5148e79c897a7b809150c1dd
Arpita Sahoo <142164739+Arpita-p-sahoo@users.noreply.github.com>
2026-08-11 10:06:22 +0000

    conversion

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -28,3 +28,39 @@ console.log( valueInNumber); // NaN(Not a number) - type of NaN is number
 
 // let someNumber = 77
 // console.log(typeof String(someNumber));
+
+/******.   Operations    ******/
+
+// let value = 3
+// let negvalue = -value
+
+// console.log(negvalue);
+
+// let a = "1";
+// let b = 2;
+
+// console.log(a + b); // 12
+// console.log(a - b); // -1
+// console.log(a * b); // 2
+// console.log(a / b); // 0.5
+// console.log(a % b); // 1
+
+/**
+ * string + number = string
+ * string - number = number
+ * string * number = number
+ * string / number = number
+ * string % number = number
+ */
+
+// console.log("1"+2+3); //123 - because string is the initial value, so all the values will be converted to string and concatenated
+// console.log(1+2+"3"); //33 - because number is the initial value, so all the values will be added and then converted to string and concatenated
+
+//conversion operation
+
+// console.log(+true); 1
+// console.log(+false); 0
+// console.log(+"1"); 1
+// console.log(+"0"); 0
+// console.log(+""); 0
+// console.log(+"undefined"); NaN
